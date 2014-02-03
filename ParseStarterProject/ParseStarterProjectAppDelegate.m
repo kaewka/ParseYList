d2237c5207103caa99fb7f875c7360305cf05ef1
Thanawat Kaewka <thanawat@Thanawats-MacBook-Air.local>
2014-02-02 22:00:56 -0800

    First commit.

--- ParseStarterProject/ParseStarterProjectAppDelegate.m
+++ ParseStarterProject/ParseStarterProjectAppDelegate.m
@@ -1,7 +1,7 @@
 
 #import "Parse/Parse.h"
+#import "TopicsTableViewController.h"
 #import "ParseStarterProjectAppDelegate.h"
-#import "MyTableController.h"
 
 @implementation ParseStarterProjectAppDelegate
 
@@ -9,12 +9,13 @@
 {
     // ****************************************************************************
     // Uncomment and fill in with your Parse credentials:
-    [Parse setApplicationId:@"APPLICATION_ID_HERE" clientKey:@"CLIENT_KEY_HERE"];
+    [Parse setApplicationId:@"PRlCTmsvTNBeypIrP63NBkJBGxG8e84yzqX7FEdj"
+                  clientKey:@"yRYOjQ999mvl3h77WmrO5Ov0FS3Dg752BwFHhuoH"];
     
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     // Override point for customization after application launch.
 
-    MyTableController *controller = [[MyTableController alloc] init];
+    TopicsTableViewController *controller = [[TopicsTableViewController alloc] init];
   
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:controller];
@@ -22,13 +23,15 @@
     
     // Let's make an object each launch, so there's definitely something to display.
     
+    /*
     PFObject *object = [PFObject objectWithClassName:@"Todo"];
-    [object setObject:@"Sample Text" forKey:@"text"];
-    [object setObject:@1 forKey:@"priority"];
+    [object setObject:@"Sample Text123" forKey:@"text"];
+    [object setObject:@3 forKey:@"priority"];
     [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
         // Refresh the table when the object is done saving.
         [controller loadObjects];
     }];
+     */
   
     return YES;
 }
